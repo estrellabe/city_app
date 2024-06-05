21afa14a508a2e2d759263bbb665fb79ce84185d
Estrella Bermejo <estrella.bermejo@alu.uclm.es>
2024-06-05 23:17:18 +0200

    Update

---
+++ routes/movies.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var router = express.Router();
+var debug = require("debug")("moviesApp:server");
+
+//Models
+var Movie = require("../models/Movie.js");
+
+var db = mongoose.connection;
+
+/* GET movies listing */
+router.get("/", function (req, res) {
+  Movie.find().then(function(movies){
+    res.status(200).json(movies)
+  }).catch(function(err){
+    res.status(500).send(err)
+  });
+});
+
+/* GET single movie by Id */
+router.get("/:id", function (req, res, next) {
+  Movie.findById(req.params.id, function (err, movieinfo) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(movieinfo);
+  });
+});
+
+/* POST a new movie*/
+router.post("/", function (req, res) {
+  Movie.create(req.body, function (err, movieinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
+});
+
+
+module.exports = router;
