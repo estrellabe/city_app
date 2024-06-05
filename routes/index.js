66ef7475e583367f941a4c492c2df83f7851acc2
Estrella Bermejo <estrella.bermejo@alu.uclm.es>
2024-06-06 01:39:51 +0200

    Update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { titulo: 'City+', mensaje: 'Hola, esta es la página de inicio (index)'});
+  res.render('index', { titulo: 'City+', mensaje: 'Esta es la página de inicio (index)'});
 });
 
 module.exports = router;
