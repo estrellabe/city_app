2e07414b95f586b0fed5bae279bf452047551347
Estrella Bermejo <estrella.bermejo@alu.uclm.es>
2024-06-10 03:40:19 +0200

    Fixing errors in the authentication process

--- routes/autenticacion.js
+++ routes/autenticacion.js
@@ -3,19 +3,37 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var dotenv = require('dotenv');
 dotenv.config();
-var passport = require('passport');
-require('../config/passport');
+var { OAuth2Client } = require('google-auth-library');
+var Usuario = require('../models/Usuario');
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email']}));
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-router.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/' }),
-    function(req, res) {
-        // Autenticación con éxito, redireccionar a la página de inicio
-        const token = jwt.sign(req.user, process.env.TOKEN_SECRETO, {expiresIn: 60 * 60 * 24});
-        res.redirect('/dashboard?token=${token}');
+// Verificacion del token de Google desde el frontend
+router.post('/google', async (req, res) => {
+    const { token } = req.body;
+    try{
+        const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID
+    });
+    
+    const { googleid, name, email} = ticket.getPayload();
+    const usuario = await Usuario.findOne({ email });
+    
+    if (usuario) {
+        const token = jwt.sign({id: usuario._id}, process.env.TOKEN_SECRETO, {expiresIn: 60 * 60 * 24});
+        res.json({ token });
+    } else {
+        usuario = new Usuario({
+            googleID : googleid,
+            nombre: name,
+            email: email,
+        });
+        await usuario.save();
+        const token = jwt.sign({ id: usuario._id }, process.env.TOKEN_SECRETO, { expiresIn: 60 * 60 * 24 });
+        res.json({ token });
     }
-);
-
-
-module.exports = router;
+} catch (error) {
+    res.status(400).json({ error: 'Error al iniciar sesión' });
+}
+});
