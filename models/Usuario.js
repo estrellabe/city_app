72bfc2536faede672b75c7552c8ae76a68dcc756
Estrella Bermejo <estrella.bermejo@alu.uclm.es>
2024-06-25 14:53:03 +0200

    Update

--- models/Usuario.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-
-var UsuarioSchema = new Schema({
-  nombre: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model("Usuario", UsuarioSchema);
